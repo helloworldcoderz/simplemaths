0ee3c6688c0dda60027da567b2122bd4578743d7
Premanand <premanand.sreedharan@gmail.com>
2022-07-02 13:43:39 +0530

    test - fix build

--- myownpytest.py
+++ myownpytest.py
@@ -2,14 +2,17 @@ import myown
 import pytest
 
 def test_add():
+    print("calling add ")
     assert myown.add(1,1) == 2
  
 def test_sub():
+    print("calling sub ")
     assert myown.sub(1,1) == 0
 
 
 def test_add2():
-    assert myown.add(1,5) == 2
-    
+    print("calling add 2 ")
+    assert myown.add(1,5) == 6
+
 
 
